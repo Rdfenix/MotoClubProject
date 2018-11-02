22b16b944e39bbe82900cd689b2f1241985782b6
rdfenix <rhoodsk8erboy@gmail.com>
2018-11-02 01:41:54 -0300

    Criando o registro de eventos

--- app/src/main/java/com/example/aiolo/testemapa/RegisterEvent.java
+++ app/src/main/java/com/example/aiolo/testemapa/RegisterEvent.java
@@ -2,12 +2,21 @@ package com.example.aiolo.testemapa;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterEvent extends AppCompatActivity {
+
+    private EditText localEvent;
+    private Button registerEvent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_event);
+
+        localEvent = findViewById(R.id.local_event);
+        registerEvent = findViewById(R.id.buttom_register_event);
+
     }
 }
